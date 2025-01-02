eaf358844d2e092eaf041c7a0dde12019ea97c97
ali221022 <shohjahon.juraev.4496@mail.ru>
2025-01-02 19:17:32 +0900

    publish homepage screen component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Home: NextPage = () => {
   return (
-  <Stack>
     <Stack className={"home-page"} >
-      <Stack>     
-        <Stack className="container">Trend Properties</Stack>
-      </Stack> 
-      <Stack>     
-        <Stack className="container">Popular Properties</Stack>
-      </Stack> 
-      <Stack>     
-        <Stack className="container">Advertisement</Stack>
-      </Stack> 
-      <Stack>     
-        <Stack className="container">Top Properties</Stack>
-      </Stack>       
-      <Stack>     
-        <Stack className="container">Top Agents</Stack>
-      </Stack>           
-    </Stack>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
   </Stack>
   );
 };
