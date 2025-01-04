226f6d52ef55a761c6b21a362a7e69c5d2d03118
ali221022 <shohjahon.juraev.4496@mail.ru>
2025-01-04 19:26:23 +0900

    BRR: feat: property list publishing

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,15 +1,10 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Box, Container, Stack } from "@mui/material";
-
+import { Container, Stack } from "@mui/material";
 
 const PropertyDetail = () => {
-    return    (
-        <div style={{ margin: "20px 0"}}>
-            <Stack className="container">
-                PROPERTY DETAIL
-            </Stack>
-        </div>
-    )
-    };
+
+    return <Container>PROPERTY DETAIL</Container>;
+  }
+
 
 export default withLayoutBasic(PropertyDetail);
